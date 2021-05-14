0d971af4ca0dfe4eaafc3c51dfa50ba76cb347fd
Sarah Johnson <sarahjohnson@Sarahs-MacBook-Air.local>
2021-05-13 17:02:34 -0700

    last commit for the night because I have other obligations

---
+++ script.js
@@ -0,0 +1,18 @@
+// Used online resources from codepen for clues to JS carousel.
+
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.querySelector(".carousel");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
